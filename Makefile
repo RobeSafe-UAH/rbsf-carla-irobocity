e7fdf509c28001ea8ccd4bd28d3a2ff6d119e4c6
miguelag99 <miguelag99@hotmail.com>
2026-06-08 11:58:12 +0200

    feat: first commit

---
+++ Makefile
@@ -0,0 +1,44 @@
+TAG_NAME := v1
+ROS_TAG := ros2_humble
+IMAGE_NAME := curso_carla_irobocity
+CONTAINER := $(IMAGE_NAME)_container
+
+UID := $(shell id -u)
+GID := $(shell id -g)
+USER_NAME := $(shell whoami)
+
+define run_docker
+	docker run -it --rm \
+		--runtime=nvidia \
+		--gpus all \
+		--net host \
+		--ipc host \
+		--ulimit memlock=-1 \
+		--ulimit stack=67108864 \
+		--name $(CONTAINER) \
+		-u $(UID):$(GID) \
+		-v $(PWD):/workspace \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-e DISPLAY=$(DISPLAY) \
+		-e TERM=xterm-256color \
+		-e NVIDIA_VISIBLE_DEVICES=all \
+		-e NVIDIA_DRIVER_CAPABILITIES=all \
+		$(IMAGE_NAME):$(TAG_NAME) \
+		bash ./deploy/entrypoint.sh
+endef
+
+build:
+	docker build deploy/ \
+		-t $(IMAGE_NAME):$(TAG_NAME) \
+		--build-arg USER=$(USER_NAME) \
+		--build-arg UID=$(UID) \
+		--build-arg GID=$(GID)
+
+attach:
+	docker exec -it $(IMAGE_NAME)_container /bin/bash -c "bash"
+
+run:
+	$(call run_docker)
+
+run_ros:
+	$(call run_docker,$(ROS_TAG), "bash")
---
+++ deploy/entrypoint.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+
+check_venv() {
+    echo "🔄 Checking virtual environment..."
+    if [ ! -d ".venv" ]; then
+        echo " .venv not found — updating virtual environment..."
+
+        if ! uv sync --link-mode=copy; then
+            echo "❌ uv sync failed, cleaning up..."
+            rm -rf .venv
+            exit 1
+        fi
+
+        echo "✅ .venv updated"
+    else
+        echo "✅ .venv found"
+    fi
+}
+
+check_cuda() {
+    echo -e "\n🔍 Checking GPU and CUDA availability..."
+
+    if ! uv run python -c "import torch" 2>/dev/null; then
+        echo -e "❌ \033[91m\033[1mFailed to import torch\033[0m"
+        echo -e "   Please check your PyTorch installation!"
+    else
+        CUDA_AVAILABLE=$(uv run python -c "import torch; print(torch.cuda.is_available())")
+
+        if [ "$CUDA_AVAILABLE" == "True" ]; then
+            echo -e "✅ \033[92m\033[1mPyTorch is working properly with the GPU.\033[0m"
+            echo -e "📍 GPU Information:"
+            uv run python -c "import torch; print(f'   - CUDA version:     {torch.version.cuda}')"
+            uv run python -c "import torch; print(f'   - Device name:      {torch.cuda.get_device_name(0)}')"
+            uv run python -c "import torch; print(f'   - Number of GPUs:   {torch.cuda.device_count()}')"
+        else
+            echo -e "❌ \033[91m\033[1mCUDA is not available!\033[0m"
+            echo -e "   Check your PyTorch installation"
+        fi
+    fi
+}
+
+###############################################################################
+
+# ========================
+# Environment
+# ========================
+
+export CARLA_ROOT=/workspace/carla
+export WORK_DIR=/workspace
+export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
+export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
+export HF_HOME=${WORK_DIR}/weights/hf
+
+
+export PYTHONPATH="${CARLA_ROOT}/PythonAPI:${CARLA_ROOT}/PythonAPI/carla:${SCENARIO_RUNNER_ROOT}:${LEADERBOARD_ROOT}:/workspace/team_code/:${PYTHONPATH:-}"
+export PATH="${HOME}/.local/bin:${PATH}"
+
+figlet -c "Robocity"
+
+check_venv
+
+echo -e "\n------------------------------------ System info ----------------------------------------\n"
+
+check_cuda
+
+
+echo -e "\n-----------------------------------------------------------------------------------------\n"
+
+/bin/bash
